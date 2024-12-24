bbbdf864a9a21c3ac80da8842e129b2de608d694
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-12-24 23:56:46 +0300

    explain code with comment lines

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,22 @@
 const bubbleSort = (arr) => {
+  // get length of the array minus 1 (loop index starts from 0 thats why minus 1)
   const n = arr.length - 1;
+  // this outter loop ensures checking the array items again and again
   for(let i = 0; i < n; i++){
+    // this variables will check if array is already sorted
     let swapped = false;
+    // this loop ensures checking all the elements and swaps if needed 
     for(let j = 0; j < n - i; j++){
       if (arr[j] > arr[j+1]){
         [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
         swapped = true;
       }
     }
+    // if already sorted exit loop
     if (!swapped)
       break;
   }
+  // return sorted array
   return arr;
 }
 
