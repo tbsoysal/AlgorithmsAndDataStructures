661a9092fbb839035239e2460488189cdac747d1
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-12-26 14:05:49 +0300

    update algorithm

--- bubblesort.js
+++ bubblesort.js
@@ -3,7 +3,7 @@ const bubbleSort = (arr) => {
   const n = arr.length; // Get the length of the array minus one
   for(let i = 0; i < n; i++){ // Outer loop to control the number of passes
     let swapped = false; // Flag to check if any swapping happened in the inner loop
-    for(let j = 0; j < n - i; j++){ // Inner loop to compare adjacent elements
+    for(let j = 0; j < n - i -1; j++){ // Inner loop to compare adjacent elements
       if (arr[j] > arr[j+1]){ // If the current element is greater than the next element
         [arr[j], arr[j+1]] = [arr[j+1], arr[j]]; // Swap the elements
         swapped = true; // Set the flag to true indicating a swap happened
